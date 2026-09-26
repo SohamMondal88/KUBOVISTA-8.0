af81367fd31f5839f9fbbc4a95bc7922b10e9e4b
SOHAM MONDAL <101863641+SohamMondal88@users.noreply.github.com>
2026-09-26 11:42:52 +0530

    Configure Firebase project kubovistas-6666

--- client/firebase-config.js
+++ client/firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase web configuration and VAPID public key are public identifiers, not Admin credentials.
 export const firebaseConfig = Object.freeze({
-  apiKey: 'AIzaSyAmgItTiIM7CteI3Org9sczED2CvtTD4LQ',
-  authDomain: 'kubovista.firebaseapp.com',
-  projectId: 'kubovista',
-  storageBucket: 'kubovista.firebasestorage.app',
-  messagingSenderId: '870066101541',
-  appId: '1:870066101541:web:cce95b19b1f39fecee8ecc'
+  apiKey: 'AIzaSyCAJo38khexZ1rOWG6Olem6LsoGxKTWSvo',
+  authDomain: 'kubovistas-6666.firebaseapp.com',
+  projectId: 'kubovistas-6666',
+  storageBucket: 'kubovistas-6666.firebasestorage.app',
+  messagingSenderId: '617913171394',
+  appId: '1:617913171394:web:3b5514a79127b84982ea5b',
+  measurementId: 'G-XNM7K6BGHR'
 });
 export const vapidKey = 'BP-Qqb4W8SVEu3e-PdDy1c-LMTGSGb6847RNDknoEV0NMX_QoPWlLROZQrfHPsA3qJj4sx0YtYJwo9WDVWSUlB4';
